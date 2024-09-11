1b06d5ef9de0383d6a4b1297a5f60cedac5a6d48
bigboyKa <amoantwikofistu@gmail.com>
2024-09-11 17:32:22 -0500

    user authentication server side added

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,62 @@
+const { PrismaAdapter } = require('@next-auth/prisma-adapter');
+const NextAuth = require('next-auth');
+const GoogleProvider = require('next-auth/providers/google');
+const CredentialsProvider = require('next-auth/providers/credentials');
+const bcrypt = require('bcrypt');
+const prisma = require('@/app/libs/prismadb');
+
+module.exports = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Changed from GOOGLE_CLIENT to GOOGLE_CLIENT_SECRET to match env variable naming convention
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid credentials');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error('Invalid credentials');
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error('Invalid credentials');
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/',
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+module.exports = {
+  authOptions,
+  default: NextAuth(authOptions),
+};
